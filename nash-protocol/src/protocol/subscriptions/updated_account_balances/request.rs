f5d5dc6523c5f8e0d4746d8fc80d2a089a535db4
Marvin Schubert <marvin@margin.de>
2021-06-09 18:24:37 +0200

    try funds sub over all pairs

--- nash-protocol/src/protocol/subscriptions/updated_account_balances/request.rs
+++ nash-protocol/src/protocol/subscriptions/updated_account_balances/request.rs
@@ -6,14 +6,14 @@ use graphql_client::GraphQLQuery;
 #[derive(Clone, Debug)]
 pub struct SubscribeAccountBalances {
     // FIXME: This should be an Option, but because of a bug, if the value is None, the request will fail.
-    pub symbol: String,
+    pub symbol: Option<String>,
 }
 
 impl SubscribeAccountBalances {
     pub fn make_query(&self) -> graphql_client::QueryBody<updated_account_balances::Variables> {
         graphql::UpdatedAccountBalances::build_query(updated_account_balances::Variables {
             payload: updated_account_balances::UpdatedAccountBalancesParams {
-                currency: Some(self.symbol.clone()),
+                currency: self.symbol.clone(),
             }
         })
     }
